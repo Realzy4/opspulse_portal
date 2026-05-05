0d2094698fed3f16bf71da414f54b40c5f05a895
Eduardo Pinho <j.eduardopinho@gmail.com>
2026-05-05 20:18:08 +0000

    v1

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import psycopg2
-# <--- Confirma se é esta a senha
+# <--- Confirma se é esta a senhasfsf
 app = Flask(__name__)
 CORS(app)
 
